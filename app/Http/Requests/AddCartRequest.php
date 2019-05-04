3aad502542281c941b8cff2a26235dfcf53c8e29
cxx <435541842@qq.com>
2019-05-04 07:47:30 +0000

    评价商品

--- app/Http/Requests/AddCartRequest.php
+++ app/Http/Requests/AddCartRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Models\ProductSku;
 
-class AddCartRequest extends FormRequest
+class AddCartRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
