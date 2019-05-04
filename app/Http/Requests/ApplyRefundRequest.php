9bf996104eeda1e1c316bf8ec27f31026a8f266a
cxx <435541842@qq.com>
2019-05-04 14:07:25 +0000

    用户界面 - 申请退款

---
+++ app/Http/Requests/ApplyRefundRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+class ApplyRefundRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'reason' => 'required',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'reason' => '原因',
+        ];
+    }
+}
