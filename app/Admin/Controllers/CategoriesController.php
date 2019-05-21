9dddc62e5bac480a1293dfc49118f23819ce8814
cxx <435541842@qq.com>
2019-05-21 15:54:15 +0000

    商品后台添加类目字段

--- app/Admin/Controllers/CategoriesController.php
+++ app/Admin/Controllers/CategoriesController.php
@@ -120,7 +120,7 @@ class CategoriesController extends Controller
         //用户输入值q获取
         $search = $request->input('q');
         $result = Category::query()
-            ->where('is_directory',true)
+            ->where('is_directory',boolval($request->input('is_directory', true)))
             ->where('name','like','%'.$search.'%')
             ->paginate();
 
